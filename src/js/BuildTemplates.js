8833103ee17bc4e9c708530fdb4e9ea2b7815f53
Alex Hoyau <a.hoyau@silexlabs.org>
2016-12-20 12:12:16 +0100

    build script compatible with node 0.12

--- src/js/BuildTemplates.js
+++ src/js/BuildTemplates.js
@@ -4,31 +4,25 @@
 // copies all ejs in the dest folder
 // and creates a json file for all the templates configs
 
-const path = require('path');
-const fs = require('fs');
-const yaml = require("js-yaml");
+var path = require('path');
+var fs = require('fs');
+var yaml = require("js-yaml");
 
-class BuilTemplates {
-  static main() {
-    console.log('Building...');
-    let src = path.resolve(__dirname, '../templates');
-    let dst = path.resolve(__dirname, '../../pub/components');
-    let allComponentsJson = {};
-    fs.readdirSync(src).map(function (file) {
-      return path.join(src, file);
-    }).filter(function (file) {
-      return fs.statSync(file).isFile()
-        && path.extname(file) === '.ejs';
-    }).forEach(function (file) {
-      let name = path.basename(file, '.ejs');
-      console.log("Building template", name);
-      let json = yaml.safeLoad(fs.readFileSync(path.resolve(src, name + '.yml')));
-      allComponentsJson[name] = json;
-      fs.createReadStream(file).pipe(fs.createWriteStream(path.resolve(dst, name + '.ejs')));
-    });
-    console.log('writing', dst + '/components.json');
-    fs.writeFileSync(dst + '/components.json', JSON.stringify(allComponentsJson));
-  }
-}
-
-BuilTemplates.main();
+console.log('Building...');
+var src = path.resolve(__dirname, '../templates');
+var dst = path.resolve(__dirname, '../../pub/components');
+var allComponentsJson = {};
+fs.readdirSync(src).map(function (file) {
+  return path.join(src, file);
+}).filter(function (file) {
+  return fs.statSync(file).isFile()
+    && path.extname(file) === '.ejs';
+}).forEach(function (file) {
+  var name = path.basename(file, '.ejs');
+  console.log("Building template", name);
+  var json = yaml.safeLoad(fs.readFileSync(path.resolve(src, name + '.yml')));
+  allComponentsJson[name] = json;
+  fs.createReadStream(file).pipe(fs.createWriteStream(path.resolve(dst, name + '.ejs')));
+});
+console.log('writing', dst + '/components.json');
+fs.writeFileSync(dst + '/components.json', JSON.stringify(allComponentsJson));
